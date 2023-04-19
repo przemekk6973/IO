712523fa43ea6451b335dc134d6497104731750e
piotrekvs <pvanselow.2000@poczta.fm>
2023-04-19 20:34:23 +0200

    Add test dsl

--- src/test/java/pl/edu/agh/io/dzikizafrykibackend/it/config/TestConfig.java
+++ src/test/java/pl/edu/agh/io/dzikizafrykibackend/it/config/TestConfig.java
@@ -1,16 +1,13 @@
 package pl.edu.agh.io.dzikizafrykibackend.it.config;
 
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import pl.edu.agh.io.dzikizafrykibackend.it.client.Retrofit2ClientFactory;
 import pl.edu.agh.io.dzikizafrykibackend.it.client.RetrofitClient;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
 
 @Configuration
 public class TestConfig {
@@ -18,11 +15,6 @@ public class TestConfig {
     @Bean
     @Scope(BeanDefinition.SCOPE_PROTOTYPE)
     public RetrofitClient retrofitClient(@Value("${local.server.port}") int port) {
-        return new Retrofit.Builder().baseUrl("http://localhost:" + port).addConverterFactory(
-                JacksonConverterFactory.create(
-                        new ObjectMapper()
-                                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false))
-                )
-                .build().create(RetrofitClient.class);
+        return new Retrofit2ClientFactory(port).createClient();
     }
 }
