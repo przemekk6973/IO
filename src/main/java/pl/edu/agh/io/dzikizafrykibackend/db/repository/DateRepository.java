c112f5fb28344bdba6f3ba7264bacfd675be9e2d
przemekk6973 <92885402+przemekk6973@users.noreply.github.com>
2023-09-15 15:33:36 +0200

    simulatedannealingtests

--- src/main/java/pl/edu/agh/io/dzikizafrykibackend/db/repository/DateRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.io.dzikizafrykibackend.db.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.edu.agh.io.dzikizafrykibackend.db.entity.DateEntity;
-
-@Repository
-public interface DateRepository extends JpaRepository<DateEntity, Integer> {
-}
--- src/main/java/pl/edu/agh/io/dzikizafrykibackend/exception/InvalidOwnerException.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.agh.io.dzikizafrykibackend.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Only course owner can make changes to the course")
-public class InvalidOwnerException extends RuntimeException {
-}
